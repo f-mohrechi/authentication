0a33c70d747c0e5e640cd7419d3f9f0c11d92b9c
f-mohrechi <f.mohrechi78@gmail.com>
2023-10-18 17:22:56 +0330

    add base name

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter basename="/authentication">
         <Routes>
           <Route element={<Signup />} path="/" />
           <Route element={<Login />} path="/login" />
